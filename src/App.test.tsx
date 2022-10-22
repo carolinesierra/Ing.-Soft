e3320bc64cfac10e4a5241d9812b1d05d6f1c540
carolinesierra <00091119@uca.edu.sv>
2022-10-21 23:25:26 -0600

    :white_check_mark: Add:test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import { describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
 
 describe('App', () => { 
-  it.todo('should render');
-  it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
-  it.todo('should have a button with text "Validar"');
-  /* Lo que demas que se les ocurra que puedan probar */
+
+  it('should rendering', () => { 
+    render(<App />);
+  });
+
+  it('should have title "Mi DUI es valido?"', () => { 
+    render(<App />)
+    expect(screen.getByText('Mi DUI es valido?')).toBeInTheDocument()
+  });
+
+  it('should have an input text', () => { 
+    render(<App />)
+    expect(screen.getByPlaceholderText('000000000')).toBeInTheDocument()
+  });
+
+  it(' should have a button with text "Validar" ', () => { 
+    render(<App />)
+    expect(screen.getByRole('button', 
+    {name: /validar/i})).toBeInTheDocument()
+  });
+
 })
